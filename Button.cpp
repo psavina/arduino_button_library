28de063dd927212499b12304b263951b434a6d24
psavina <pierpaolo.savina@protonmail.com>
2018-07-01 20:55:38 +0200

    simplified the library

--- Button.cpp
+++ Button.cpp
@@ -1,40 +1,76 @@
+/*
+ * ===========================================================
+ * Button library
+ * ===========================================================
+ * author: psavina
+ * version: 1.0
+ * date: 2018-07-01
+ * -----------------------------------------------------------
+ * A simple library to handle buttons.
+ * There are 4 different states:
+ * - Pressed:      Check if a button was pressed.
+ * - Released:     Check if a button was released.
+ * - Pressing:     Check if a button is currently pressed.
+ * - NoKeyPressed: Check if a button is not pressed.
+ * ----------------------------------------------------------
+ *
+ * Copyright (c) 2018 psavina
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+
+
 #include "Button.h"
 
 Button::Button(unsigned short int ButtonPin){
   _ButtonPin = ButtonPin;
-  _ButtonState = 0;
   _LastButtonState = 0;
-  _KeyPressed = 0;
-  _KeyReleased = 0;
-  _KeyPressing = 0;
-  _NoKeyPressed = 0;
+  _IsPressed = 0;
 }
 
 void Button::UpdateState(){
   int CurrentState = digitalRead(_ButtonPin);
+  
   if(CurrentState != _LastButtonState){
-    if(_KeyPressed == 0 && _KeyPressing == 0){
-      _KeyPressed = 1;
+    if(_IsPressed == 0){
+      _IsPressed = 1;
+      _LastButtonState = CurrentState;
+      return;
     }
 
-    if(_KeyPressing == 1){
-      _KeyReleased = 1;
-      _KeyPressing = 0;
+    if(_IsPressed == 3){
+      _IsPressed = 2;
+      _LastButtonState = CurrentState;
+      return;
     }
   }
   else{
-    if(_KeyPressed == 1){
-      _KeyPressed = 0;
-      _KeyPressing = 1;
+    if(_IsPressed == 1){
+      _IsPressed = 3;
+      _LastButtonState = CurrentState;
+      return;
     }
-    if(_KeyReleased == 1){
-      _KeyReleased = 0;
-      _KeyPressing = 0;  
+    if(_IsPressed == 2){
+      _IsPressed = 0;
+      _LastButtonState = CurrentState;
+      return;
     }  
   }
-  if(_KeyReleased == 0 && _KeyPressing == 0 && _KeyPressed == 0)
-    _NoKeyPressed = 1;
-  else
-    _NoKeyPressed = 0;
+
+  _IsPressed = 0;
   _LastButtonState = CurrentState;
+  return;
 }
--- Button.h
+++ Button.h
@@ -1,3 +1,36 @@
+/*
+ * ===========================================================
+ * Button library
+ * ===========================================================
+ * author: psavina
+ * version: 1.0
+ * date: 2018-07-01
+ * -----------------------------------------------------------
+ * A simple library to handle buttons.
+ * There are 4 different states:
+ * - Pressed:      Check if a button was pressed.
+ * - Released:     Check if a button was released.
+ * - Pressing:     Check if a button is currently pressed.
+ * - NoKeyPressed: Check if a button is not pressed.
+ * ----------------------------------------------------------
+ *
+ * Copyright (c) 2018 psavina
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA 
+ */
+
 #ifndef __BUTTON_LIB_H_
 #define __BUTTON_LIB_H_
 
@@ -6,18 +39,15 @@ private:
   unsigned short int       _ButtonPin;
   unsigned short int       _ButtonState;
   unsigned short int       _LastButtonState;
-  unsigned short int       _KeyPressed;
-  unsigned short int       _KeyReleased;
-  unsigned short int       _KeyPressing;
-  unsigned short int       _NoKeyPressed;
+  unsigned short int       _IsPressed;
+  void                     UpdateState();
 
 public:
   Button(unsigned short int ButtonPin);
-  void      UpdateState();
-  bool      Pressed(){return _KeyPressed == 1;}
-  bool      Released(){return _KeyReleased == 1;}
-  bool      Pressing(){return _KeyPressing == 1;}
-  bool      NoPressed(){return _NoKeyPressed == 1;}
+  bool      Pressed(){return   _IsPressed == 1;}
+  bool      Released(){return  _IsPressed == 2;}
+  bool      Pressing(){return  _IsPressed == 3;}
+  bool      NoPressed(){return _IsPressed == 0;}
 };
 
 
